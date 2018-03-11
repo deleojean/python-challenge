d48f57d0ea0f0a53ca5d1025beb27d46e0c84fe5
Developer <developer@Jeans-MBP.fios-router.home>
2018-03-11 00:05:01 -0500

    Add new method and change parameters to dict

--- SharedAssets/Translator.py
+++ SharedAssets/Translator.py
@@ -5,9 +5,19 @@ class Translator:
     def __init__(self):
         self._allowed_values = []
 
-    def validate_parameters(self, name, parameter, argument, valid_values):
-        if not argument in valid_values:
-            print(f"ERROR [{self._stage}] Allowed '{name}({parameter})' arguments: '{valid_values}'")
+    def collect_metrics(self, **kwargs):
+        metrics   = []
+        pass_rate = 0
+
+        fail = kwargs['total'] - kwargs['counter']
+        pass_rate = (kwargs['counter'] / kwargs['total']) * 100 if kwargs['counter'] > 0 else pass_rate
+        metrics.extend([{'name': kwargs['key'], 'passfail': f"{kwargs['counter']}/{fail}", 'rate': f"{round(pass_rate, 2)}%"}])
+
+        return metrics
+
+    def validate_parameters(self, **kwargs):
+        if not kwargs['argument'] in kwargs['valid_values']:
+            print(f"ERROR [{self._stage}] Allowed '{kwargs['name']}({kwargs['parameter']})' arguments: '{kwargs['valid_values']}'")
             sys.exit(1)
 
     def translate(self):
