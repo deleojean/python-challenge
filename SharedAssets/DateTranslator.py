dd7a3302e3d3bfcafca7e4872a572fc012ee3256
Developer <developer@Jeans-MBP.fios-router.home>
2018-03-10 22:47:35 -0500

    Create DateTranslator class

---
+++ SharedAssets/DateTranslator.py
@@ -0,0 +1,50 @@
+import csv
+import datetime
+import sys
+from Translator import Translator
+
+class DateTranslator(Translator):
+
+    def __init__(self):
+        self._allowed_values = ['%b-%y', '%b-%Y', '%y-%b'] # add strfttime code as needed
+
+    def translate(self, dataset=[], key='Date', format='%b-%y'):
+        new_dataset = []
+
+        super().validate_parameters(name='translate', parameter='format', argument=format, valid_values=self._allowed_values)
+
+        try:
+            for index, data in enumerate(dataset):
+                for strftime in self._allowed_values:
+                    try:
+                        formatted_date = datetime.datetime.strptime(data[key], strftime).strftime(format)
+
+                        data[key] = formatted_date
+                        break
+                    except ValueError:
+                        if self._allowed_values.index(strftime) == len(self._allowed_values) - 1:
+                            print(f"ERROR [{Translator._stage}] Unhandled data format: '[{index}][date]{data[key]}'")
+
+                        continue
+
+                new_dataset.append(data)
+        except KeyError as err:
+            print("ERROR [{Translator._stage}] Key value not found in collection:", err)
+            sys.exit(1)
+
+        return new_dataset
+
+def main():
+    dataset = []
+
+    with open('../PyBank/raw_data/budget_data_1.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader: dataset.append(row)
+
+    date = DateTranslator()
+    dataset = date.translate(dataset)
+
+    for data in dataset:
+        print(data)
+
+if __name__ == '__main__': main()
